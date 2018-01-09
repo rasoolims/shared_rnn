bd9120ec88ab2f36a5d138f7e96a546b8217bc4b
Mohammad Sadegh Rasooli <rasooli.ms@gmail.com>
2018-01-09 14:03:26 -0500

    debugging!

--- src/network.py
+++ src/network.py
@@ -143,7 +143,8 @@ class Network:
         words, pos_tags, chars, langs, signs, masks = mini_batch
         h_out = self.rnn_mlp(mini_batch, True)[-1]
         t_out = dy.transpose(dy.reshape(h_out, (h_out.dim()[0][0], h_out.dim()[1])))
-        print 'dims', t_out.dim(), len(langs)
+        dots = t_out * t_out
+        print 'dims', t_out.dim(), len(langs), dots.dim()
         #norm_vals = self.norms(t_out).value()
 
         k = float(t_out.dim()[0][0] - len(chars))
